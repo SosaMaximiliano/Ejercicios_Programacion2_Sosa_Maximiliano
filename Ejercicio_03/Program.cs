9c64c18ad5b76eca2e8538978f795aedb0012656
Maximiliano Sosa <maxisosa25@hotmail.com>
2022-08-23 18:44:36 -0300

    Ejercicio3

--- Ejercicio_03/Program.cs
+++ Ejercicio_03/Program.cs
@@ -6,7 +6,31 @@ namespace Ejercicio_03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num;
+            string sNum;
+            int contador = 0;
+
+            Console.Write("Numero: ");
+
+            sNum = Console.ReadLine();
+            int.TryParse(sNum, out num);
+
+            for (int i = 1; i < num; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        contador += i;
+                    }
+                }
+
+                if (contador == i)
+                {
+                    Console.WriteLine(i);
+                }
+                contador = 0;
+            }
         }
     }
 }
