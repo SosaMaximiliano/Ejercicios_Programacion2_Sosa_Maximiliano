4f36441d896712313ec50656747858e723433ca8
Maximiliano Sosa <maxisosa25@hotmail.com>
2022-08-22 22:33:37 -0300

    Clase01

---
+++ Ejercicio_01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ejercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int acumulador = 0;
+            int num;
+            int max = 0;
+            int min = 0;
+            float promedio = 0f;
+            string sNum;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Numero {i+1}:");
+                sNum = Console.ReadLine();
+                int.TryParse(sNum, out num);
+
+                if (i==0)
+                {
+                    max = num;
+                    min = num;
+                }
+
+                if (num < min)
+                {
+                    min = num;
+                }
+
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                acumulador += num;
+            }
+
+            promedio = acumulador / 5;
+
+            Console.WriteLine($"El minimo es: {min}");
+            Console.WriteLine($"El maximo es: {max}");
+            Console.WriteLine($"El promedio es: {promedio}");
+
+        }
+    }
+}
---
+++ Ejercicio_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sNum;
+            int num;
+            double cuadrado;
+            double cubo;
+
+            Console.WriteLine("Numero:");
+            sNum = Console.ReadLine();
+            int.TryParse(sNum, out num);
+            if (num > 0)
+            {
+                cuadrado = Math.Pow(num, 2);
+                cubo = Math.Pow(num, 3);
+
+                Console.WriteLine($"El cuadrado de {num} es: {cuadrado}");
+                Console.WriteLine($"El cubo de {num} es: {cubo}");
+            }
+            else
+            {
+                Console.WriteLine("ERROR. ¡Reingresar número!");
+            }
+
+        }
+    }
+}
